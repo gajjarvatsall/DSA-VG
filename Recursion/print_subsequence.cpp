f062a173956823446689baefe20696465c1bfc3c
gajjarvatsall <vatsalgajjar84@gmail.com>
2025-03-17 22:13:38 +0530

    subseqence done

---
+++ Recursion/print_subsequence.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printSubSequence(string str, string output, int index)
+{
+    if (index == str.length())
+    {
+        cout << output << endl;
+        return;
+    }
+    printSubSequence(str, output + str[index], index + 1);
+    printSubSequence(str, output, index + 1); 
+}
+int main()
+{
+    string str = "abc";
+    string output = "";
+    int index = 0;
+    printSubSequence(str, output, index);
+    return 0;
+}
